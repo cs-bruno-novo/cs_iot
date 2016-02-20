354d04124bcbfd50c8b60f353aa654af2a04cd4a
Victor Lima <victorlima@Victors-MacBook-Pro.local>
2016-02-20 13:47:47 -0200

    estrutura inicial do projeto arduino

---
+++ genius.ino
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
+{
+	printf("")
+}
+
+char *build_array()
+{
+	
+}
---
+++ genius_test.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <stdio.h>
+
+void test_should_create_initial_array()
+{
+	// chama função que cria array
+
+	// garante que o array não está vazio
+	assert( actual != NULL );
+	assert( )
+}
